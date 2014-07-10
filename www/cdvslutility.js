405c845321c2cd0a20de6e700700697bebaffc6c
slooky <slookyllc@gmail.com>
2014-07-10 13:24:05 +0200

    Update function call
    Change arguments type (jsonobject -> single argument)

--- www/cdvslutility.js
+++ www/cdvslutility.js
@@ -1,3 +1,10 @@
+cordova.define("co.slooky.plugins.cdvslutility.CDVSLUtility", function(require, exports, module) { /*
+ *
+ * Copyright 2014 SLOOKY LLC
+ * http://slooky.co
+ *
+*/
+
 var cdvslutility = {
 	openExternalURL : function (url, title, message, positiveButtonTitle, negativeButtonTitle, successCallback, errorCallback) {
 		cordova.exec(
@@ -5,14 +12,15 @@ var cdvslutility = {
             errorCallback, // error callback function
             'CDVSLUtility', // mapped to our native Java class called "CDSLUtility"
             'openExternalURL', // with this action name
-            [{                  // and this array of custom arguments
+            [                 // and this object of custom arguments
                 "url": url,
                 "title": title,
                 "message": message,
                 "positiveButtonTitle": positiveButtonTitle,
                 "negativeButtonTitle": negativeButtonTitle
-            }]
+            ]
         );
 	}
 }
 module.exports = cdvslutility;
+});
